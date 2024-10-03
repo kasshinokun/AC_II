751e156fbadb8c00e7a96be33f7a428f1845d0de
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-10-02 22:28:30 -0300

    の更新

---
+++ test_arduino.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int check_string(string str) {
+   
+    if str.size()%3==0{
+        return 1;
+    }
+    eles{
+        return 0;
+    }
+}
+char[] convert_char_array(string str){
+    
+    if check_string(str)==1{
+        char[] convert=str.split(3);
+        return convert;
+    }else{
+        return nome;
+    }
+
+}
