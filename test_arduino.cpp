45e16b6bbc1cec7f3bb8b31effe808975030e991
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-10-02 23:13:33 -0300

    test_arduino.cpp の更新

--- test_arduino.cpp
+++ test_arduino.cpp
@@ -1,9 +1,15 @@
+//Código exemplo para o Arduino
+
+//Criado em 02-10-2024
+//Revisão 1_02_10_2024
+
+
 #include <iostream>
 using namespace std;
 
 int check_string(string str) {
    
-    if str.size()%3==0{
+    if str.size()%3==0 and str.size()%96==0{
         return 1;
     }
     eles{
@@ -13,10 +19,40 @@ int check_string(string str) {
 char[] convert_char_array(string str){
     
     if check_string(str)==1{
-        char[] convert=str.split(3);
-        return convert;
+        
+        char arr[str.length() + 1]; 
+
+	       return strcpy(arr, str.c_str()); 
+        
     }else{
-        return nome;
+
+        return char arr[0] = '\0';
+
     }
 
 }
+void execute_operations(string str){
+
+    char arr[str.length() + 1];   
+    if strcpy(arr,convert_char_array(str))!='\0'{
+     
+    }else{
+        cout<<"Erro"<<endl;
+    }
+
+
+}
+
+//portas
+void porta_not(char a){}
+void porta_and(char a,char b){}
+void porta_or(char a,char b){}
+void porta_xor(char a,char b){}
+void porta_nand(char a,char b){}
+void porta_nor(char a,char b){}
+void porta_xnor(char a,char b){}
+
+//1 lógico
+
+
+//0 lógico
