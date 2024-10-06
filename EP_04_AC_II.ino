5d8f2e5aa858cdf0430e4026f21e60e5a985e615
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-10-05 22:39:25 -0300

    Criar molde para versão final

---
+++ EP_04_AC_II.ino
@@ -0,0 +1,54 @@
+//Exercício Prático 4 de Arquitetura de Computadores II 
+//Criado em 05-10-2024
+//Revisão 1_05_10_2024
+
+
+
+
+
+
+//Validação do Char Recebido 
+bool validate_char(char a){
+    return isHexadecimalDigit(a);
+}
+
+//Split da String em array de Char
+
+
+
+
+
+//Portas lógicas 
+
+
+
+
+
+
+//MUX da ULA
+
+
+
+
+//Configuração da simulação 
+void setup(){
+    //Iniciar o Serial
+
+
+    //Iniciar LED's
+
+
+    //Demais funções
+
+
+}
+//Loop de processos
+void loop(){
+    //Roda rotina para tratar a string recebida
+
+
+
+}
+
+
+
