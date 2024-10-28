d343a4eef1e626c00ac4c20593fb7925907ed975
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-10-29 06:23:15 +0900

    Create executa_linux_float.sh

---
+++ EP_05_2024_2/Revisao_envio/ENVIO/1M_instrucoes/test_c/Float/executa_linux_float.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Pressione ENTER para iniciar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Atribuicao i=j\nPressione ENTER para continuar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Atribuicao i=i+3\nPressione ENTER para continuar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Atribuicao i=i*3\nPressione ENTER para continuar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Atribuicao i=i+j\nPressione ENTER para continuar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Atribuicao i=i*j\nPressione ENTER para continuar.....'
+  #Executa comando
+
+  #Aguarda por 5 segundos
+  read -t 5 -s -r -p  $'Processo Finalizado\nPressione ENTER para encerrar.....'
