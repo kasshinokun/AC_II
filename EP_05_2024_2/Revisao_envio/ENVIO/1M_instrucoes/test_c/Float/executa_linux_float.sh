0737c51e1e33135b83be8f1ad5be4cb790771512
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-10-29 07:18:35 +0900

    Update executa_linux_float.sh

--- EP_05_2024_2/Revisao_envio/ENVIO/1M_instrucoes/test_c/Float/executa_linux_float.sh
+++ EP_05_2024_2/Revisao_envio/ENVIO/1M_instrucoes/test_c/Float/executa_linux_float.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
   #Executa comando
-
+  FILE="main_float_1.c"
+  gcc -Wall -W "$FILE" && ./a.out
+  echo
   #Aguarda por 5 segundos
   read -t 5 -s -r -p  $'Pressione ENTER para iniciar.....'
   #Executa comando
-
+  FILE="main_float_2.c"
+  gcc -Wall -W "$FILE" && ./b.out
+  echo
   #Aguarda por 5 segundos
   read -t 5 -s -r -p  $'Atribuicao i=j\nPressione ENTER para continuar.....'
   #Executa comando
-
+  FILE="main_float_3.c"
+  gcc -Wall -W "$FILE" && ./c.out
+  echo
   #Aguarda por 5 segundos
   read -t 5 -s -r -p  $'Atribuicao i=i+3\nPressione ENTER para continuar.....'
   #Executa comando
-
+  FILE="main_float_4.c"
+  gcc -Wall -W "$FILE" && ./d.out
+  echo
   #Aguarda por 5 segundos
   read -t 5 -s -r -p  $'Atribuicao i=i*3\nPressione ENTER para continuar.....'
   #Executa comando
-
-  #Aguarda por 5 segundos
-  read -t 5 -s -r -p  $'Atribuicao i=i+j\nPressione ENTER para continuar.....'
-  #Executa comando
-
-  #Aguarda por 5 segundos
-  read -t 5 -s -r -p  $'Atribuicao i=i*j\nPressione ENTER para continuar.....'
-  #Executa comando
-
+  FILE="main_float_5.c"
+  gcc -Wall -W "$FILE" && ./e.out
+  echo
   #Aguarda por 5 segundos
   read -t 5 -s -r -p  $'Processo Finalizado\nPressione ENTER para encerrar.....'
+  
